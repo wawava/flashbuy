9b73cbdd9b172daf47d4fb9cee83ec846b3203f2
towne <tangqii@qq.com>
2014-07-09 16:30:02 +0800

    fix ui

--- FNFlashBuy/FNAppDelegate.m
+++ FNFlashBuy/FNAppDelegate.m
@@ -42,25 +42,16 @@
 	
 	// Add a first section
 	[self.slideoutController addSectionWithTitle:@"FIRST SECTION"];
-	
-	NSString* icon1 = @"icon1.png";
-	if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
-		icon1 = @"icon1b.png";
-	}
-	NSString* icon2 = @"icon2.png";
-	if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
-		icon2 = @"icon2b.png";
-	}
 
-	[self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:1 withTitle:@"首页" andIcon:icon1];
+	[self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:1 withTitle:@"首页" andIcon:nil];
 
-	[self.slideoutController addViewControllerToLastSection:[FNMyOrderVC new] tagged:2 withTitle:@"我的订单" andIcon:icon2];
+	[self.slideoutController addViewControllerToLastSection:[FNMyOrderVC new] tagged:2 withTitle:@"我的订单" andIcon:@"Member2_1"];
     
-	[self.slideoutController addViewControllerToLastSection:[FNMyAddrVC new] tagged:3 withTitle:@"地址管理" andIcon:icon2];
+	[self.slideoutController addViewControllerToLastSection:[FNMyAddrVC new] tagged:3 withTitle:@"地址管理" andIcon:@"Member2_2"];
     
-	[self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:4 withTitle:@"关于我们" andIcon:icon2];
+	[self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:4 withTitle:@"关于我们" andIcon:@"Member2_3"];
     
-    [self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:5 withTitle:@"检查更新" andIcon:icon2];
+    [self.slideoutController addViewControllerToLastSection:[FNHomeVC new] tagged:5 withTitle:@"检查更新" andIcon:@"Member2_4"];
     
 	
 	// Add a second section
